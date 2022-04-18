c43f8c11cdc056ffacec361577527107ab3c390d
pawel <finestedm@gmail.com>
2022-04-18 23:34:50 +0200

    basic functions working. sqrt and square left to implement ans some stylistic work. Them the extra work for optional stuff

--- myscript.js
+++ myscript.js
@@ -4,20 +4,36 @@ const displayHistoryLineOperator = document.getElementById('history-line-operato
 
 
 buttonList = document.querySelectorAll('#keyboard button');
-buttonList.forEach(element => {
+buttonList.forEach(element => {                             // add event listener  for all buttons
     element.addEventListener("click", (e) => {
-        if (e.target.attributes.class.nodeValue == 'operational') {
-            var arg1 = parseFloat(displayHistoryLine.innerHTML);
-            var arg2 = parseFloat(displayCurrentLine.innerHTML);
-            var operationType = e.target.innerHTML;
+        if (e.target.attributes.class.nodeValue == 'operational') {  // if the button is operational then:
+            var arg1 = parseFloat(displayHistoryLine.innerHTML);     // get arg1 from history line on display
+            var arg2 = parseFloat(displayCurrentLine.innerHTML);     // get arg2 from current line on display
+            var operationType = displayHistoryLineOperator.innerHTML;   // get type of oparation from display (previously selected operation (before arg2))
             var result = doOperation(operationType, arg1, arg2);
             console.log(operationType, arg1, arg2);
             displayHistoryLine.innerHTML = result;
             displayHistoryLineOperator.innerHTML = e.target.innerHTML;
             displayCurrentLine.innerHTML = '';
+        } else if (e.target.attributes.class.nodeValue == 'special') {
+            if (e.target.id == 'equals') {
+                var arg1 = parseFloat(displayHistoryLine.innerHTML);     // get arg1 from history line on display
+                var arg2 = parseFloat(displayCurrentLine.innerHTML);     // get arg2 from current line on display
+                var operationType = displayHistoryLineOperator.innerHTML;   // get type of oparation from display (previously selected operation (before arg2))
+                var result = doOperation(operationType, arg1, arg2);
+                displayCurrentLine.innerHTML = result;
+                displayHistoryLineOperator.innerHTML = e.target.innerHTML;
+                displayHistoryLine.innerHTML = '';
+            } else if (e.target.id == 'clear') {
+                displayCurrentLine.innerHTML = '';
+                displayHistoryLineOperator.innerHTML = '';
+                displayHistoryLine.innerHTML = '';
+
+            }
         } else {
-            addTextToCurrentLine(e.target.firstChild.data);
+            addTextToCurrentLine(e.target.firstChild.data);         // if pressed button is digit then add the digit to the current display
         }
+
     });
 });
 
@@ -26,18 +42,12 @@ function addTextToCurrentLine(text) {
     console.log(text);
 }
 
-
-
-function doOperation(operationType, arg1 = 0, arg2) {
-    if (arg1 == 0) {
+function doOperation(operationType, arg1 = 0, arg2 = 0) {
+    if ((isNaN(arg1)) || (operationType === '')) {
         return arg2;
-    } else if (arg2 == '') {
-        console.log('tesdee')
-
+    } else if (isNaN(arg2)) {
         return arg1;
     } else {
-        console.log('tesd')
-
         if (operationType == '+') {
             return arg1 + arg2
         } else if (operationType == '-') {
@@ -45,16 +55,8 @@ function doOperation(operationType, arg1 = 0, arg2) {
         } else if (operationType == '*') {
             return arg1 * arg2
         } else if (operationType == '/') {
-            return arg1 / arg2
+            var result = (arg2 === 0) ? 'we are doomed!' : (arg1 / arg2);
+            return result;
         }
     }
-
 }
-
-function moveToHistoryLine(argument, operator) {
-    console.log(operator.innerHTML)
-    displayHistoryLine.innerHTML = argument + operator;
-    displayCurrentLine.innerHTML = ''
-}
-
-// displayHistoryLine.innerHTML = doOperation(e.target.id, displayHistoryLine.innerHTML, displayCurrentLine.innerHTML) :
